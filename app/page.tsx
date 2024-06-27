8b5d7e341695dff488fab01b85b5ee366e38b37e
Pedro Arruda <pedro.arruda@allcomsistemas.com.br>
2024-06-27 11:09:20 -0300

    feat: add product list and product item components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Header from './_components/header';
 import Search from './_components/search';
 import CategoryList from './_components/category-list';
 import Image from 'next/image';
+import ProductList from './_components/products-list';
+import { Button } from './_components/ui/button';
+import { ChevronRightIcon } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -26,6 +29,20 @@ export default function Home() {
           sizes="100vw"
           quality={100}
         />
+      </div>
+
+      <div className="pt-6 space-y-4">
+        <div className="px-5 flex justify-between items-center">
+          <h2 className="font-semibold">Pedidos recomendados</h2>
+          <Button
+            variant="ghost"
+            className="text-primary p-0 hover:bg-transparent h-fit"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
       </div>
     </>
   );
