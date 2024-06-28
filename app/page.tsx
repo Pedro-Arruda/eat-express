bbb8071c109c9db50d4d745cc6ee20f10433ef75
Pedro Arruda <pedro.arruda@allcomsistemas.com.br>
2024-06-28 10:43:18 -0300

    feat: add recommended recommended restaurants page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { ChevronRightIcon } from 'lucide-react';
 import { db } from './_lib/prisma';
 import PromoBanner from './_components/promo-banner';
 import RestaurantList from './_components/restaurant-list';
+import Link from 'next/link';
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -46,7 +47,7 @@ const Home = async () => {
 
       <div className="pt-6 space-y-4">
         <div className="px-5 flex justify-between items-center">
-          <h2 className="font-semibold">Pedidos recomendados</h2>
+          <h2 className="font-semibold">Produtos recomendados</h2>
           <Button
             variant="ghost"
             className="text-primary p-0 hover:bg-transparent h-fit"
@@ -68,13 +69,15 @@ const Home = async () => {
       <div className="py-6 space-y-4">
         <div className="px-5 flex justify-between items-center">
           <h2 className="font-semibold">Restaurantes recomendados</h2>
-          <Button
-            variant="ghost"
-            className="text-primary p-0 hover:bg-transparent h-fit"
-          >
-            Ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href="/restaurants/recommended">
+            <Button
+              variant="ghost"
+              className="text-primary p-0 hover:bg-transparent h-fit"
+            >
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <RestaurantList />
       </div>
