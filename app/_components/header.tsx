67a1fe85ae766fa26c5c7bfaea55c0f5f0d34b2b
Pedro Arruda <pedro.arruda@allcomsistemas.com.br>
2024-06-28 10:46:12 -0300

    fix: add div relative around logo image

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,22 @@
-import Image from "next/image";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { MenuIcon } from 'lucide-react';
 
 const Header = () => {
-    return ( 
+  return (
     <div className="flex justify-between pt-6 px-5">
-        <Image src="/logo.png" alt="Logo Food" width={100} height={30}/>
-        <Button size="icon" variant="outline" className="bg-transparent border-none">
-            <MenuIcon />
-        </Button>
-    </div> 
-    );
-}
- 
+      <div className="relative h-[30px] w-[100px]">
+        <Image src="/logo.png" alt="Logo Food" fill className="object-cover" />
+      </div>
+      <Button
+        size="icon"
+        variant="outline"
+        className="bg-transparent border-none"
+      >
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+};
+
 export default Header;
