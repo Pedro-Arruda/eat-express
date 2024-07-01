a7bc37e0fd7090469ce6bf99b0e90743c9b8813f
Pedro Arruda <pedro.arruda@allcomsistemas.com.br>
2024-07-01 15:29:40 -0300

    fix: ensure restaurant params is envolved by a suspense

--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -7,42 +7,45 @@ import { searchForRestaurant } from './_actions/search';
 import RestaurantItem from '../_components/restaurant-item';
 import Header from '../_components/header';
 
-const Restaurants = () => {
+const RestaurantsComponent = () => {
   const searchParams = useSearchParams();
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-
   const searchFor = searchParams.get('search');
 
   useEffect(() => {
     const fetchRestaurants = async () => {
       if (!searchFor) return;
-
       const foundRestaurants = await searchForRestaurant(searchFor);
       setRestaurants(foundRestaurants);
     };
-
     fetchRestaurants();
-  }, [searchParams]);
+  }, [searchFor]);
 
   if (!searchFor) return notFound();
 
   return (
+    <div className="py-6 px-5">
+      <h2 className="text-lg font-semibold mb-6">Restaurantes Encontrados</h2>
+      <div className="flex flex-col gap-6 w-full">
+        {restaurants.map((restaurant) => (
+          <RestaurantItem
+            restaurant={restaurant}
+            key={restaurant.id}
+            className="min-w-full max-w-full"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Restaurants = () => {
+  return (
     <>
       <Header />
-      <div className="py-6 px-5">
-        <h2 className="text-lg font-semibold mb-6">Restaurantes Encontrados</h2>
-        <div className="flex flex-col gap-6 w-full">
-          <Suspense fallback={<div>Loading...</div>}>
-            {restaurants.map((restaunt) => (
-              <RestaurantItem
-                restaurant={restaunt}
-                key={restaunt.id}
-                className="min-w-full max-w-full"
-              />
-            ))}
-          </Suspense>
-        </div>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RestaurantsComponent />
+      </Suspense>
     </>
   );
 };
